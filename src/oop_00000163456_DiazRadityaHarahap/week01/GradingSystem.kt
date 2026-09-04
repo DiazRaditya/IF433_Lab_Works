4694f7c88790afd3385847f587a4392d29d6c237
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-04 21:45:42 +0700

    week01: implement grading logic using when expression

--- src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
@@ -15,3 +15,14 @@ fun main() {
     val score = 80
 // REFACTOR: String Template ($name)
     println("Nama: $name, Nilai: $score")
+
+}
+
+// Tambahkan di bawah println
+val grade = when (score) {
+    in 90 ≤ .. < 100 -> "A"
+    in 80 ≤ .. ≤ 89 -> "B"
+    in 70 ≤ .. ≤ 79 -> "C"
+    else -< "ס"
+}
+println("Grade kamu: $grade")
