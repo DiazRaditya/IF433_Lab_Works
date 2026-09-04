8fdadfca713257613963ceb02499730340041eb1
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-04 21:47:14 +0700

    week01: implement null safety check

--- src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
@@ -32,3 +32,9 @@ fun calculateStatus (score: Int) if (score > 75) "Lulus" else "Tidak Lulus"
 
 // Panggil DI DALAM main()
 println("Status: ${calculateStatus (score)}")
+
+// Tambahkan di akhir main()
+val studentId: String? = null
+// Jika null, gunakan nilai default 0
+val idLength = studentId?.length ?: 0
+println("Panjang ID: $idLength")
