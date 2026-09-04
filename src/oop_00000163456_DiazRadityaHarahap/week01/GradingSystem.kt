0b2da5ab5ab5f72fe97a866f581ee5bef7f39fb3
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-04 21:44:25 +0700

    week01: refactor to use val and string templates

--- src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week01/GradingSystem.kt
@@ -1,2 +1,17 @@
 package oop_00000163456_DiazRadityaHarahap.week01
 
+package oop_001_johnthor.week01 // Pastikan package sesuai
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+// Concatenation gaya lama
+    11 println ("Nama: " + name + Nilai:+score)
+
+}
+
+fun main() {
+// REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "John Thor"
+    val score = 80
+// REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
