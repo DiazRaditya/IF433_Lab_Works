68a2562187d44de1e1000ad8041425084ab4d601
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-04 22:41:40 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000163456_DiazRadityaHarahap/week01/GameStore.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week01/GameStore.kt
@@ -15,3 +15,20 @@ fun main() {
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
+
+fun main() {
+    val gameTitle = "streamkw"
+    val price = 600000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - (price * discount / 100)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Game: $title")
+    println("Harga Akhir: Rp $finalPrice")
+}
