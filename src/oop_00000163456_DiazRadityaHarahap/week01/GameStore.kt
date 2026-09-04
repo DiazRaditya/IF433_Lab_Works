e49dafb88f0a02742c1c45e1daf83c5541c23e9d
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-04 22:39:44 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000163456_DiazRadityaHarahap/week01/GameStore.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week01/GameStore.kt
@@ -5,3 +5,13 @@ fun main(){
     val discount = calculateDiscount (price)
     val finalprice = price -(price * discount / 100)
 }
+
+fun main() {
+    val gameTitle = "streamkw"
+    val price = 600000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - (price * discount / 100)
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
