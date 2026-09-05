b8cb471219e58c3e12bdbd721c59b6fdb2319355
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-05 10:30:53 +0700

    week02: add warning validation in init block

--- src/oop_00000163456_DiazRadityaHarahap/week02/Student.kt
+++ src/oop_00000163456_DiazRadityaHarahap/week02/Student.kt
@@ -8,3 +8,18 @@ class Student (new
 ){
 // Body class kosong dulu
 }
+
+class Student (val name: String, val nim: String, var major: String) { new
+    init {
+        new
+// Validasi Sederhana: Cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+
+        }
+    }
+}
+
