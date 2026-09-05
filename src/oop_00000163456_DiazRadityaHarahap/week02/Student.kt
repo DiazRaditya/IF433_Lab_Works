e0ac8182b35c2830ab7a91c4295772590804f13b
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-05 10:19:22 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000163456_DiazRadityaHarahap/week02/Student.kt
@@ -0,0 +1,10 @@
+package oop_00000163456_DiazRadityaHarahap.week02
+
+package oop_001_johnthor.week02
+class Student (new
+               val name: String,
+               val nim: String,
+               var major: String
+){
+// Body class kosong dulu
+}
