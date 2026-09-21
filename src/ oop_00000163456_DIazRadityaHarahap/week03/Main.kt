271cef6dcac206e632ebeaa17675f71ec756fa94
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-22 01:21:38 +0700

    week03: add private property

--- src/ oop_00000163456_DIazRadityaHarahap/week03/Main.kt
+++ src/ oop_00000163456_DIazRadityaHarahap/week03/Main.kt
@@ -6,3 +6,18 @@ fun main() {
     e.salary = 5000 // Ini akan meledakkan program Anda
 
 }
+
+// Hanya bisa diakses di file/class ini saja
+
+private var performanceRating: Int = 3
+
+fun increasePerformance() {
+    performanceRating++
+    println("Kinerja Sname meningkat! Rating: SperformanceRating")
+}
+// Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar raha
+// kecuali kita buat function khusus untuk print.
+fun printStatus() {
+    println("Karyawan: Sname, Rating: SperformanceRating")
+
+}
