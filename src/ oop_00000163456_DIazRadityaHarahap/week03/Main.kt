2b9480b56a722ba9c9eb7ae1776ae32a4148738a
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-22 01:13:54 +0700

    week03: simulate stack overflow error in setter

---
+++ src/ oop_00000163456_DIazRadityaHarahap/week03/Main.kt
@@ -0,0 +1,8 @@
+package ` oop_00000163456_DIazRadityaHarahap`.week03
+
+fun main() {
+    val e = Employee("budi"
+    println("Test Error")
+    e.salary = 5000 // Ini akan meledakkan program Anda
+
+}
