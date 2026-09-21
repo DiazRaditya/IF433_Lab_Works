25a7d1618865eea0987fb2b42f55a0e09ca9c11e
Diaz Raditya Harahap <diaz.raditya@student.umn.ac.id>
2026-09-22 01:18:48 +0700

    week03: fix setter using backing field

--- src/ oop_00000163456_DIazRadityaHarahap/week03/Main.kt
+++ src/ oop_00000163456_DIazRadityaHarahap/week03/Main.kt
@@ -2,7 +2,7 @@ package ` oop_00000163456_DIazRadityaHarahap`.week03
 
 fun main() {
     val e = Employee("budi"
-    println("Test Error")
+    println("--- Test Error ---")
     e.salary = 5000 // Ini akan meledakkan program Anda
 
 }
--- src/ oop_00000163456_DIazRadityaHarahap/week03/employee.kt
+++ src/ oop_00000163456_DIazRadityaHarahap/week03/employee.kt
@@ -1,11 +1,13 @@
 package ` oop_00000163456_DIazRadityaHarahap`.week03
 class Employee (val name: String)
 {
+
     var salary: Int = 0
-            set(value) {
-            println("Mencoba set gaji ke: $value")
-            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
-            // Kita memanggil setter di dalam setter (Recursive)
-            this.salary = value
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field 0// Gunakan field, bukan this.salary
+            } else {
+                field = value // Gunakan field untuk assign nilai asli}
+            }
         }
-}
